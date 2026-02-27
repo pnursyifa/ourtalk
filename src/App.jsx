d5fac6a535eb58cb333fa64aaeab05d6cc82bcdf
pnursyifa <pnursyifa@gmail.com>
2026-02-27 09:45:57 +0700

    chore: add more dependencies

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className='app-container'>
+        <header>
+          {/* <Navigation>
+          </Navigation> */}
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
